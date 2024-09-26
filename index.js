5f3454202234ba0ce84f479b175232ef1b4dce6d
Soltime5476 <129920421+Soltime5476@users.noreply.github.com>
2024-09-26 17:19:07 +0800

    Added code to randomize color on hover

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-const container = document.querySelector("#container")
-const squareNum = 16
-for (let i = 0; i < squareNum; i++) {
-  for (let j = 0; j < squareNum; j++) {
-    const element = document.createElement("div")
-    element.className = "grid"
-    element.id = `square${i}-${j}`
-    container.appendChild(element)
+function getRandomColor() {
+  let hexStr = "#"
+  for (let i = 0; i < 6; i++) {
+    hexStr += Math.floor(Math.random() * 16).toString(16);
+  }
+  return hexStr
+}
+
+function mouseEnterCallback(event) {
+  const grid = event.target
+  grid.style.backgroundColor = getRandomColor()
+}
+
+function mouseLeaveCallback(event) {
+
+}
+
+function generateGrid(gridSize) {
+  const container = document.querySelector("#container")
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      const element = document.createElement("div")
+      element.className = "grid"
+      element.id = `square${i}-${j}`
+      element.addEventListener("mouseenter", mouseEnterCallback)
+      element.addEventListener("mouseleave", mouseLeaveCallback)
+      container.appendChild(element)
+    }
   }
 }
+
+const gridDefaultSize = 16
+generateGrid(gridDefaultSize);
+
