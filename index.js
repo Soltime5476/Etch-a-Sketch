1a706d6833442bb8116d0097ea39a5f9c7e884da
Soltime5476 <129920421+Soltime5476@users.noreply.github.com>
2024-09-26 18:59:12 +0800

    Reordered buttons and added switch between using normal and random rgb colour

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+let usingRandomColor = false
+const containerSize = 640;
+const gridDefaultSize = 16
+const defaultColor = "darkred"
+
 function getRandomColor() {
   let hexStr = "#"
   for (let i = 0; i < 6; i++) {
@@ -8,27 +13,62 @@ function getRandomColor() {
 
 function mouseEnterCallback(event) {
   const grid = event.target
-  grid.style.backgroundColor = getRandomColor()
-}
-
-function mouseLeaveCallback(event) {
-
+  if (usingRandomColor) {
+    grid.style.backgroundColor = getRandomColor()
+  }
+  else {
+    grid.style.backgroundColor = defaultColor
+  }
 }
 
 function generateGrid(gridSize) {
+  // reset the button modes
+  normalModeButton.disabled = true
+  randomModeButton.disabled = false
+
   const container = document.querySelector("#container")
+  container.replaceChildren()
+  const sideLength = containerSize / gridSize
   for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
       const element = document.createElement("div")
       element.className = "grid"
       element.id = `square${i}-${j}`
+      element.style.width = `${sideLength}px`
+      element.style.height = `${sideLength}px`
+      element.style.borderWidth = `${Math.min(sideLength / 20, 4)}px`
       element.addEventListener("mouseenter", mouseEnterCallback)
-      element.addEventListener("mouseleave", mouseLeaveCallback)
       container.appendChild(element)
     }
   }
 }
 
-const gridDefaultSize = 16
+function promptNewGrid() {
+  const gridSize = parseInt(prompt("Enter the new grid size."));
+  if (isNaN(gridSize) || gridSize <= 0 || gridSize > 100) {
+    alert("Grid size must be between 1 and 100.")
+    return
+  }
+  generateGrid(gridSize)
+}
+
+function clearGrid() {
+  const container = document.querySelector("#container")
+  for (let i of container.childNodes) {
+    i.style.removeProperty("background-color")
+  }
+}
+
+const newGridButton = document.querySelector("button#new-grid")
+const clearButton = document.querySelector("button#clear")
+const normalModeButton = document.querySelector("button#normal-mode")
+normalModeButton.disabled = true;
+const randomModeButton = document.querySelector("button#random-mode")
+
+newGridButton.addEventListener("click", promptNewGrid)
+clearButton.addEventListener("click", clearGrid)
+normalModeButton.addEventListener("click", (e) => {e.target.disabled = true; randomModeButton.disabled = false; usingRandomColor = false})
+randomModeButton.addEventListener("click", (e) => {e.target.disabled = true; normalModeButton.disabled = false; usingRandomColor = true})
+
 generateGrid(gridDefaultSize);
 
