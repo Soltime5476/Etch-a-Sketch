239d24c9e6d0dbd32dcfccd3e71208686ece0b6d
Soltime5476 <129920421+Soltime5476@users.noreply.github.com>
2024-09-26 16:10:48 +0800

    Added readme and code to create square divs

---
+++ index.js
@@ -0,0 +1,9 @@
+const container = document.querySelector("#container")
+const squareNum = 16
+for (let i = 0; i < squareNum; i++) {
+  for (let j = 0; j < squareNum; j++) {
+    const element = document.createElement("div")
+    element.id = `square${i}-${j}` 
+    container.appendChild(element)
+  }
+}
